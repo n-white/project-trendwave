f0a8686d1e802121d833dd79cab1ba9db806c343
n-white <neil.r.whitehead@gmail.com>
2016-07-21 12:24:51 -0700

    able to load the ccsv file

--- server/database.js
+++ server/database.js
@@ -1,33 +1,33 @@
 var Sequelize = require('Sequelize');
 var sequelize = new Sequelize('trendWaveDB', 'root', 'cake');
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
 var Trends = sequelize.define('Trends', {
  name: Sequelize.STRING
 });
 
 var Twitter_Sentiments = sequelize.define('Twitter_Sentiments', {
-   neutral: Sequelize.DECIMAL,
-   positive: Sequelize.DECIMAL,
-   negative: Sequelize.DECIMAL
+  neutral: Sequelize.DECIMAL,
+  positive: Sequelize.DECIMAL,
+  negative: Sequelize.DECIMAL
 });
 
 var FB_Sentiments = sequelize.define('FB_Sentiments', {
-   status_id: Sequelize.STRING,
-   status_message: Sequelize.STRING,
-   link_name: Sequelize.STRING,
-   status_type: Sequelize.STRING,
-   status_link: Sequelize.STRING,
-   status_published: Sequelize.DATE,
-   num_reactions: Sequelize.INTEGER,
-   num_comments: Sequelize.INTEGER,
-   num_shares: Sequelize.INTEGER,
-   num_likes: Sequelize.INTEGER,
-   num_loves: Sequelize.INTEGER,
-   num_wows: Sequelize.INTEGER,
-   num_hahas: Sequelize.INTEGER,
-   num_sads: Sequelize.INTEGER,
-   num_angrys: Sequelize.INTEGER
+  status_id: Sequelize.STRING,
+  status_message: Sequelize.STRING(2000),
+  link_name: Sequelize.STRING,
+  status_type: Sequelize.STRING,
+  status_link: Sequelize.STRING,
+  status_published: Sequelize.STRING,
+  num_reactions: Sequelize.INTEGER,
+  num_comments: Sequelize.INTEGER,
+  num_shares: Sequelize.INTEGER,
+  num_likes: Sequelize.INTEGER,
+  num_loves: Sequelize.INTEGER,
+  num_wows: Sequelize.INTEGER,
+  num_hahas: Sequelize.INTEGER,
+  num_sads: Sequelize.INTEGER,
+  num_angrys: Sequelize.INTEGER
 });
 
 // var Trend_Popularity = sequelize.define('Trend_Popularity', {
@@ -35,13 +35,17 @@ var FB_Sentiments = sequelize.define('FB_Sentiments', {
 //   time: Sequelize.DATE
 // });
 
-Trends.hasMany(Twitter_Sentiments);
+// Trends.hasMany(Twitter_Sentiments);
 
 // FB_Sentiments.hasMany(Trends);
 // Trend_Popularity.hasMany(Trends);
 
-Twitter_Sentiments.sync().then(Trends.sync().then(FB_Sentiments.sync()));
-// Trends.sync().then(Twitter_Sentiments.sync().then(FB_Sentiments.sync(Trend_Popularity.sync())));
+// Twitter_Sentiments.sync().then(Trends.sync().then(FB_Sentiments.sync().then(function() {
+// 	mysql.query('alter table FB_Sentiments drop column updatedAt;');
+// 	mysql.query('alter table FB_Sentiments drop column createdAt;')
+// })));
+
+Trends.sync().then(Twitter_Sentiments.sync().then(FB_Sentiments.sync()));
 
 exports.Trends = Trends;
 exports.Twitter_Sentiments = Twitter_Sentiments;
